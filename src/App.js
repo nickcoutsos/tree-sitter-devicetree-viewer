54f322d90c8f8821c6ed22acc781719decf1a942
Nick Coutsos <nick@coutsos.com>
2022-11-24 22:30:25 -0500

    Dynamic swapping of language wasm

--- src/App.js
+++ src/App.js
@@ -8,32 +8,84 @@ import Tree from './Tree'
 
 let _parser
 
-async function getParser () {
+const languageBuilds = {
+  main: {
+    name: 'Official',
+    description: 'WASM build from main tree-sitter-devicetree branch',
+    ref: 'https://github.com/joelspadin/tree-sitter-devicetree/tree/main',
+    path: './tree-sitter-devicetree.wasm'
+  },
+  mine: {
+    name: 'Experimental',
+    description: 'Custom build from pending pull requests or experiments',
+    ref: 'https://github.com/nickcoutsos/tree-sitter-devicetree/tree/pending',
+    path: './tree-sitter-devicetree-pending.wasm'
+  }
+}
+
+async function getParser (buildId) {
   if (!_parser) {
     await window.TreeSitter.init()
-    const language = await window.TreeSitter.Language.load('./tree-sitter-devicetree.wasm')
     _parser = new window.TreeSitter()
-    _parser.setLanguage(language)
+  }
+
+  const build = languageBuilds[buildId]
+  if (!build.language) {
+    build.language = await window.TreeSitter.Language.load(build.path)
+  }
+  if (_parser.language !== build.language) {
+    _parser.setLanguage(build.language)
   }
 
   return _parser
 }
 
+function BuildSelector({ value, onChange }) {
+  return (
+    <div style={{
+      position: 'absolute',
+      top: '20px',
+      right: '20px',
+      zIndex: '10'
+    }}>
+      <label>
+        Devicetree build:&nbsp;
+        <select
+          value={value}
+          onChange={e => onChange(e.target.value)}
+        >
+          {Object.keys(languageBuilds).map(buildId => (
+            <option
+              key={buildId}
+              value={buildId}
+              title={languageBuilds[buildId].description}
+            >
+              {languageBuilds[buildId].name}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
+  )
+}
+
 
 function App() {
+  const [buildId, setBuildId] = useState('main')
   const [sample, setSample] = useState(sampleSource)
   const [tree, setTree] = useState(null)
   const [selectedNode, setSelectedNode] = useState(null)
 
   useEffect(() => {
     (async function () {
-      const parser = await getParser()
+      const parser = await getParser(buildId)
       setTree(parser.parse(sample))
     })()
-  }, [sample, setTree])
+  }, [buildId, sample, setTree])
 
   return (
     <>
+      <BuildSelector value={buildId} onChange={setBuildId} />
       {tree && (
         <div style={{ flexBasis: 'fit-content' }}>
           <Tree
