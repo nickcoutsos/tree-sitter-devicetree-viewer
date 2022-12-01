1273fdfcc544ae93f3415f7bc5941ec51014366c
Nick Coutsos <nick@coutsos.com>
2022-11-30 21:27:21 -0500

    Support discovery of multiple children with the same field

--- src/Tree.js
+++ src/Tree.js
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import compact from 'lodash/compact'
 import styles from './tree.module.css'
 
-function Node ({ node, selectedId, onSelect }) {
+function mapChildFields (node) {
+  const map = {}
+
+  // Iteration is done with a cursor here because only TreeCursor will directly
+  // tell us what field is associated with the current node. The node itself
+  // will not tell us about its own field, and Node.childForFieldName(field)
+  // will only tell us about a single child with the specified field.
+  const cursor = node.walk()
+  cursor.gotoFirstChild()
+
+  do {
+    const isNamed = cursor.currentNode().isNamed()
+    const field = cursor.currentFieldName()
+    if (isNamed && field) {
+      map[cursor.nodeId] = field
+    }
+  } while (cursor.gotoNextSibling())
+
+  return map
+}
+
+function Node ({ node, field, selectedId, onSelect }) {
   const [isCollapsed, setCollapsed] = useState(false)
   const nodeChildren = node.namedChildren
   const isSelected = node.id === selectedId
   const isNamed = node.isNamed()
   const hasErrors = node.hasError()
-  const field = node.tree.language.fields.find(field => (
-    node.parent?.childForFieldName(field)?.id === node.id && field
-  ))
+
+  const childFieldMap = useMemo(() => mapChildFields(node), [node])
 
   return (
     <div className={styles.listContainer}>
@@ -35,7 +55,12 @@ function Node ({ node, selectedId, onSelect }) {
       <ul>
         {!isCollapsed && nodeChildren.map((childNode, i) => (
           <li key={i}>
-            <Node node={childNode} selectedId={selectedId} onSelect={onSelect} />
+            <Node
+              node={childNode}
+              field={childFieldMap[childNode.id]}
+              selectedId={selectedId}
+              onSelect={onSelect}
+            />
           </li>
         ))}
       </ul>
@@ -48,6 +73,7 @@ function Tree ({ tree, selectedNode, onSelect }) {
     <div className={styles.treeContainer}>
       <Node
         node={tree.rootNode}
+        field={null}
         selectedId={selectedNode?.id}
         onSelect={onSelect}
       />
